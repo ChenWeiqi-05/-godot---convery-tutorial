06f57a9445bca24b418b953b218b23dad6d5d8d5
来自犬世界的君子 <2292007239@qq.com>
2026-04-28 15:27:34 +0800

    Add tile map world generation with texture atlas
    - Add _create_world() function to generate 20x20 tile grid
    - Configure TileSet with coveyAsset.png as atlas source
    - Set up TileMapLayer with the new tile set

--- Documents/convery-tutorial/source/scene/game_scene.gd
+++ Documents/convery-tutorial/source/scene/game_scene.gd
@@ -2,5 +2,16 @@ extends Node2D
 
 @onready var tile_map_layer: TileMapLayer = $TileMapLayer
 
+func _ready() -> void:
+	_create_world()
 
-@onready var camera_2d: Camera2D = $Camera2D
+
+func _create_world() -> void:
+	for x in range(0,20):
+		for y in range(0,20):
+			var cell := Vector2i(x,y)
+			tile_map_layer.set_sell(cell,0,Vector2i.ZERO) 
+			
+			
+			
+			
